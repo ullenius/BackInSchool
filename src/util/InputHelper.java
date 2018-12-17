90711c62a3e82f7cc634315c5e2eb5ae67c04c77
Anosh D. Ullenius <anosh@anosh.se>
2018-12-17 23:18:50 +0100

    Refactoring

--- src/util/InputHelper.java
+++ src/util/InputHelper.java
@@ -1,42 +1,42 @@
 package util;
 import java.util.Scanner;
-
-// klass med statiska hjälpmetoder för input/output
 /**
- * 
+ *
+ * Helper class with methods used for user I/O
+ * using Scanner
+ *
  * Based on code by Lars Jelleryd, 2018
- * 
+ *
  * @author Modification Anosh. D Ullenius <anosh@anosh.se>
  *
  */
-public class InputHelper {
-
-	private final Scanner sc;
-
-	public InputHelper() {
-
-            sc = new Scanner(System.in);
-	}
-
-	public String getText(String message) {
-
-		System.out.print(message); // print user defined message
-		StringBuilder input = new StringBuilder(sc.nextLine()); 
-		// använder StringBuilder, för att undvika skit i String Pool
-		sc.nextLine(); // tömmer buffern
-
-		return input.toString();
-	}
-
-	public int getInt(String message) {
-
-	//	System.out.println("getint med parameter"); // DEBUG
-
-		System.out.print(message); // print user defined message
-		int input = sc.nextInt();
-		sc.nextLine(); // tömmer buffern
-
-		return input;
-
-	}
+public final class InputHelper {
+    
+    private final Scanner sc;
+    public InputHelper() {
+        sc = new Scanner(System.in);
+    }
+    
+    /**
+     * Prints user defined message and returns a String
+     * Uses StringBuilder to avoid putting stuff in the
+     * String pool
+     * 
+     * @param message
+     * @return 
+     */
+    public String getText(String message) {
+        System.out.print(message);
+        StringBuilder input = new StringBuilder(sc.nextLine());
+        sc.nextLine(); // clears buffer
+        return input.toString();
+    }
+    
+    public int getInt(String message) {
+        System.out.print(message);
+        int input = sc.nextInt();
+        sc.nextLine();
+        
+        return input;
+    }
 }
